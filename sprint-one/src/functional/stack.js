be59ac0dc7de7902dd6e8f0062e37e796a6621ae
Thomas Lyons <talyons22@gmail.com>
2019-12-16 22:19:39 -0800

    stack.js works - need to remove use of this

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,17 +2,28 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  someInstance.storage = {};
+  someInstance.nextKey = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    this.storage[this.nextKey] = value;
+    this.nextKey++;
   };
 
   someInstance.pop = function() {
+    var item = this.storage[this.nextKey - 1];
+    delete this.storage[this.nextKey - 1];
+    if (this.nextKey >= 1) {
+      this.nextKey--;
+    }
+    return item;
   };
 
   someInstance.size = function() {
+    return this.nextKey;
   };
 
   return someInstance;
 };
+
