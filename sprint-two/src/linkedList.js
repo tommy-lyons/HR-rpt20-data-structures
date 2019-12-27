e3ae135b2d9fbfebf2804d500445e1741ac14250
Thomas Lyons <talyons22@gmail.com>
2019-12-27 12:30:24 -0800

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,40 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (this.head === null) {
+      this.head = node;
+    }
+    if (this.tail !== null) {
+      this.tail.next = node;
+    }
+    this.tail = node;
   };
 
   list.removeHead = function() {
+    // move head pointer to next item in our list after original head
+    var newHead = list.head;
+    list.head = newHead.next;
+    // whatever was "first" value before will get cancelled
+    // return value of the former head
+    return newHead.value;
   };
 
   list.contains = function(target) {
+    var current = list.head;
+
+    while (current) {
+      if (current.value === target) {
+        return true;
+      } else {
+        current = current.next;
+      }
+    }
+
+    return false;
+
   };
 
   return list;
