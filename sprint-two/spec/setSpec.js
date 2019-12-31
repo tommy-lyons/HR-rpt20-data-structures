fe04b33cdc1c1a16750ce3b8b8fbcd37294a5b9b
Thomas Lyons <talyons22@gmail.com>
2019-12-31 13:35:32 -0800

    Add new tests to setSpec.js

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -11,7 +11,7 @@ describe('set', function() {
     expect(set.remove).to.be.a('function');
   });
 
-  it('should add values to a set', function() {
+  it('should add string values to a set', function() {
     set.add('Susan Sarandon');
     set.add('Danny Glover');
     expect(set.contains('Danny Glover')).to.equal(true);
@@ -22,6 +22,20 @@ describe('set', function() {
     set.add('Mel Gibson');
     set.remove('Mel Gibson');
     expect(set.contains('Mel Gibson')).to.equal(false);
+  });
+
+  it('should add numerical values to a set', function() {
+    set.add(2);
+    set.add(5);
+    expect(set.contains(2)).to.equal(true);
+    expect(set.contains(5)).to.equal(true);
+  });
+
+  it('should add boolean values to a set', function() {
+    set.add(true);
+    set.add(false);
+    expect(set.contains(true)).to.equal(true);
+    expect(set.contains(false)).to.equal(true);
   });
 
 });
