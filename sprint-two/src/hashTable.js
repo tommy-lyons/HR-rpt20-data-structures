211cd5de94331ed41aae22bae45faacfbd416d8d
Thomas Lyons <talyons22@gmail.com>
2020-01-04 15:57:07 -0800

    Compete Hash Table Resize

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,13 +2,35 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
+  //additional code for new test added
   if (!arguments[1]) {
     return 'You forgot to pass in a value argument';
   }
+
   // for advanced content - want to add code to change the length of the limitedArray to keep within 25 and 75 percent of total
+  if (this._count >= 0.75 * this._limit) {
+    this._limit *= 2;
+    var holdingArray = [];
+
+    this._storage.each(function(bucket, index) {
+      if (bucket) {
+        for (var i = 0; i < bucket.length; i++) {
+          holdingArray.push(bucket[i]);
+        }
+      }
+    });
+    this._count = 0;
+    this._storage = LimitedArray(this._limit);
+
+    for (var i = 0; i < holdingArray.length; i++) {
+      this.insert(holdingArray[i][0], holdingArray[i][1]);
+    }
+  }
+
   // produce index using GetIndexBelowMaxForKey
   var index = getIndexBelowMaxForKey(k, this._limit);
 
@@ -21,6 +43,7 @@ HashTable.prototype.insert = function(k, v) {
     this._storage.set(index, []);
     var bucket = this._storage.get(index);
     bucket.push([k, v]);
+    this._count++;
   } else { // if there is already a bucket, then look at that bucket, and verify whether it has our key
     var bucket = this._storage.get(index);
     for (var i = 0; i < bucket.length; i++) {
@@ -32,6 +55,7 @@ HashTable.prototype.insert = function(k, v) {
     }
     // otherwise if it doesn't have the key then push the tuple (key, value) to the bucket
     bucket.push([k, v]);
+    this._count++;
   }
 };
 
@@ -51,17 +75,41 @@ HashTable.prototype.retrieve = function(k) {
 };
 
 HashTable.prototype.remove = function(k) {
+  // resize if storage gets too small
+  if (this._count < 0.25 * this._limit) {
+    this._limit /= 2;
+    var holdingArray = [];
+
+    this._storage.each(function(bucket, index) {
+      if (bucket) {
+        for (var i = 0; i < bucket.length; i++) {
+          holdingArray.push(bucket[i]);
+        }
+      }
+    });
+    this._count = 0;
+    this._storage = LimitedArray(this._limit);
+
+    for (var i = 0; i < holdingArray.length; i++) {
+      this.insert(holdingArray[i][0], holdingArray[i][1]);
+    }
+  }
+
   var index = getIndexBelowMaxForKey(k, this._limit);
   // if bucket doesn't exist, then exit out?
   // otherwise, iterate thru bucket length
   // if tuple[i][0] === k, then we take out tuple by bucket.splice(i, 1);
   var bucket = this._storage.get(index);
+
   if (!bucket) {
     return 'no value found';
   } else {
     for (var i = 0; i < bucket.length; i++) {
       if (bucket[i][0] === k) {
+        console.log(this._count);
+        this._count--;
         return bucket.splice(i, 1);
+
       }
     }
   }
